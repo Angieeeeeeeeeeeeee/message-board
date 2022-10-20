571121aa54e749d561511f36bc57b99f6d0649e2
Angie <nhpss91020@gmail.com>
2022-10-20 18:07:14 +0800

    linkAPI

--- src/MSG-Board/Board.js
+++ src/MSG-Board/Board.js
@@ -1,21 +1,38 @@
 import './MSGboard.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MSG from './MSG'
+
+const API_URL = 'https://student-json-api.lidemy.me/comments'
 
 
 function Board() {
+  const [message, setMessage] = useState([]);
+  const [apiError, setApiError] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data)
+      })
+
+      .catch(err => {
+        setApiError(err.message)
+      })
+  }, [])
+
   return (
     <div className='web'>
-    <div className='board'>
-      <h1 className='tilte' >留言板</h1>
-      <form typeof='form' className='board-form' >
-        <input typeof='text' className='board-input' />
-        <button className='board-summit'>送出留言</button>
-      </form>
-      <div className='board-list'>
-        <MSG author={'Angie'} time='2022-10-19'>留言內容</MSG>
+      <div className='board'>
+        <h1 className='title' >留言板</h1>
+        <form typeof='form' className='board-form' >
+          <input typeof='text' className='board-input' />
+          <button className='board-summit'>送出留言</button>
+        </form>
+        <div className='board-list'>
+          <MSG author={'Angie'} time='2022-10-19'>留言內容</MSG>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
